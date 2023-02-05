992a50444b2f8525db24e9084f43f03e916e290c
mr232432 <mr232432@falmouth.ac.uk>
2023-02-05 15:20:19 +0000

    feat: worked more on syncing plants

--- Assets/coherence/baked/Archetypes.gen.cs
+++ Assets/coherence/baked/Archetypes.gen.cs
@@ -14,6 +14,12 @@ namespace Coherence.Generated
 		public const int Player_d8263f41a789c7b4d99d257a64a29b99Index = 1;
 		public const int Spawner_4c242586da6a31949973a28c5fabc675Index = 2;
 		public const int Enemy_8e3461cf8d4e4834688cfb7eaef98857Index = 3;
+		public const int First__char_32_Person__char_32_Player_5b66f4ea00578924390ab3ef8489d129Index = 4;
+		public const int Lily__char_32_Pad_e6b57ddcc936ef24db26545e3576fe23Index = 5;
+		public const int Plant_d765de643a6a3a84481a9718beb599b3Index = 6;
+		public const int Venus__char_32_Fly__char_32_Trap_cd0989dbe6b806a499067cd427b33d57Index = 7;
+		public const int Spores_200a876feaaaf244589706ac69e6dc16Index = 8;
+		public const int Roots_4e2bbc787b16997458ad85cd28b6e92dIndex = 9;
 
 		public static Dictionary<string, int> IndexForName = new Dictionary<string, int>() {
 			{ "DefencePoint_e2ce5010b5b9e6e4dbcc44fba32a7f6e", 0 },
@@ -23,6 +29,18 @@ namespace Coherence.Generated
 			{ "Spawner_4c242586da6a31949973a28c5fabc675", 2 },
 
 			{ "Enemy_8e3461cf8d4e4834688cfb7eaef98857", 3 },
+
+			{ "First__char_32_Person__char_32_Player_5b66f4ea00578924390ab3ef8489d129", 4 },
+
+			{ "Lily__char_32_Pad_e6b57ddcc936ef24db26545e3576fe23", 5 },
+
+			{ "Plant_d765de643a6a3a84481a9718beb599b3", 6 },
+
+			{ "Venus__char_32_Fly__char_32_Trap_cd0989dbe6b806a499067cd427b33d57", 7 },
+
+			{ "Spores_200a876feaaaf244589706ac69e6dc16", 8 },
+
+			{ "Roots_4e2bbc787b16997458ad85cd28b6e92d", 9 },
 
 		};
 	}
